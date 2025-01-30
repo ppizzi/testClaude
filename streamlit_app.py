3a6852d36b767f011f8a362f1f5f071c0d7f6537
ppizzi <43932233+ppizzi@users.noreply.github.com>
2025-01-30 16:39:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import json
+from PIL import Image
+import PIL
+import os
+import base64
+import boto3
+from botocore.exceptions import ClientError
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# -- functions --
+
+def encode_image(image):
+    #st.write("converting image to b64")
+    try:
+        encoded_image = base64.b64encode(image.read()).decode("utf-8") #< works with uploaded file from st.upload_file, but not with an image from PIL or from os.open
+    except:
+        encoded_image = base64.b64encode(image).decode("utf-8")
+    #encoded_image
+    #st.write("converted")
+    
+    return encoded_image
+
+
+
+
+####--- main page ---###
+st.title("Test your message to Claude API via Bedrock")
